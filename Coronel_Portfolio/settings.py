b0aac8c68350df8b473867f34ee7d763e00a4886
ShaniaTechCode <shaniacoronel70@gmail.com>
2025-12-06 22:31:32 +0800

    Updated settings

--- Coronel_Portfolio/settings.py
+++ Coronel_Portfolio/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middle.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'Coronel_Portfolio.urls'
